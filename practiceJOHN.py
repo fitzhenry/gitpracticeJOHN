e0f558e3bb3e7dec5ffd7bfcb52f764d811bf1b3
John Fitzhenry <johnfitzhenry@Johns-MacBook-Pro.local>
2020-02-26 11:34:05 -0800

    Fixing errors

--- practiceJOHN.py
+++ practiceJOHN.py
@@ -5,22 +5,21 @@
 
 import random as rand
 def getNRandom(n):
-	'''takes in an integer and returns a list of n random integers between 1 and 10, inclusive'''
+    '''takes in an integer and returns a list of n random integers between 1 and 10, inclusive'''
     number_list = []
     for i in range(n):
-        number_list.append(rand.randint(1,10)
+        number_list.append(rand.randint(1,10))
     return number_list
 
 def multiplyRandom(numbers):
-	'''takes in a list of n numbers and returns the product of the numbers'''
-    pass
-        product = 1
-        for i in numbers:
-            product = product*i
-        return product
+    '''takes in a list of n numbers and returns the product of the numbers'''
+    product = 1
+    for i in numbers:
+        product = product*i
+    return product
 
 def main():
-	print(multiplyRandom(getNRandom(10))
+    print(multiplyRandom(getNRandom(10)))
 
 if __name__ == "__main__":
-	main()
+    main()
