cc85ea0a05e320e48f9f3c4edb7363aec11d12cf
John Fitzhenry <johnfitzhenry@Johns-MacBook-Pro.local>
2020-02-26 11:14:52 -0800

    added points

--- practiceJOHN.py
+++ practiceJOHN.py
@@ -1,7 +1,7 @@
 # Partner 1: John Robert Fitzlovely
 # partner 2: Ethan Bankowski
 ######################
-# Assignment Name: GitHub practice
+# Assignment Name: GitHub practice - 20 points
 
 def getNRandom(n):
 	'''takes in an integer and returns a list of n random integers between 1 and 10, inclusive'''
